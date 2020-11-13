12020c54f5f98117e52f7763e75cccfa4551ca9e
Ritwik Sarkar <43826920+ritwiksarkar11@users.noreply.github.com>
2020-11-13 15:53:52 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'TOPSIS-Ritwik-101803624',        
+  packages = ['TOPSIS-Ritwik-101803624'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Topsis Assignment',   
+  author = 'Ritwik Sarkar',                  
+  author_email = 'rsarkar_be18@thapar.edu',      
+  url = 'https://github.com/user/reponame',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['TOPSIS', 'PYTHON', 'ASSIGNMENT'],  
+  install_requires=[            
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
